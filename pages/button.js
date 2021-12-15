fa503dec2f2ec76f3f771ebc06674ed74cb23545
Aandreeww <91228348+Aandreeww@users.noreply.github.com>
2021-12-16 09:25:29 +1000

    Update button.js

--- pages/button.js
+++ pages/button.js
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="button.js"></script>
-    <title>Document</title>
-    <link rel="stylesheet" href="../styles/button.css">
-  </head>
-  <body>
-      <p class="mouse_target" onmouseover="console.log(1)" onmouseout="console.log(1)"></p>
-      <div class="mouse_click" onclick="element_click()"></div>
-      <div class="form_check">
-          <form name="script_1">
-              <input class="button_click" type="button" onclick="">
-          </form>
-      </div>
-  </body>
-  </html>
+function element_click() {
+    let element = document.querySelector(".mouse_click")
+    element.style.backgroundColor = "red"
+    console.log(2)
+}
